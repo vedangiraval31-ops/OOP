bf6dd6a5d842f12d467681232b6f61a54e373923
vedangiraval31-ops <vedangiraval31@gmail.com>
2026-01-08 21:00:17 +0530

    Add files via upload

---
+++ Practical1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+   class Practical1
+ {
+      public static void main(String[] args)
+ {
+            Scanner sc = new Scanner(System.in);
+
+          System.out.print("Enter distance in meters: ");
+
+        double meters = sc.nextDouble();
+
+         double feet = meters * 3.28084;
+
+            System.out.printf("Distance in feet: %.2f", feet);
+   
+        sc.close();
+    }
+}
